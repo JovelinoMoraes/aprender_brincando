cc84b5a664d8009619e1f9cd947d24da2e7bc389
Jovelino Moraes <jovelinodev@gmail.com>
2026-07-31 22:40:44 -0300

    implementado mais animais

--- aprender_brincando/lib/data/animals_data.dart
+++ aprender_brincando/lib/data/animals_data.dart
@@ -16,4 +16,19 @@ List<Animal> animais = [
     imagem: 'assets/images/cavalo.png',
     som: 'audio/cavalo.mp3',
   ),
+  const Animal(
+    nome: 'Porco',
+    imagem: 'assets/images/porco.png',
+    som: 'audio/porco.mp3',
+  ),
+  const Animal(
+    nome: 'Dinossauro',
+    imagem: 'assets/images/dinossauro.png',
+    som: 'audio/dinossauro.mp3',
+  ),
+  const Animal(
+    nome: 'Gato',
+    imagem: 'assets/images/gato.png',
+    som: 'audio/gato.mp3',
+  ),
 ];
--- aprender_brincando/lib/screens/animal_screen.dart
+++ aprender_brincando/lib/screens/animal_screen.dart
@@ -25,154 +25,154 @@ class _AnimalScreenState extends State<AnimalScreen> {
       backgroundColor: Colors.pink[200],
 
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.pink[300],
-                shape: BoxShape.circle,
-                boxShadow: [
-                  const BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
+        child: Container(
+          decoration: BoxDecoration(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.pink[300],
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    const BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 4,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
 
-              width: 500,
-              child: Image.asset(
-                animalAtual.imagem,
-                width: 1000,
-                height: 400,
-                fit: BoxFit.contain,
-              ),
-            ), // imagem do animal
+                width: 500,
+                child: Image.asset(
+                  animalAtual.imagem,
+                  width: 1000,
+                  height: 400,
+                  fit: BoxFit.contain,
+                ),
+              ), // imagem do animal
 
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.pink[200],
-                borderRadius: BorderRadius.all(Radius.circular(50)),
-                boxShadow: [
-                  const BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 8,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
-              width: 350,
-              height: 100,
-              child: FittedBox(
-                child: Text(
-                  animalAtual.nome, // nome do animal
-                  style: const TextStyle(
-                    // estilização do nome do animal
-                    fontSize: 60,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    shadows: [
-                      Shadow(offset: Offset(-1, -1), color: Colors.black),
-                      Shadow(offset: Offset(1, -1), color: Colors.black),
-                      Shadow(offset: Offset(1, 1), color: Colors.black),
-                      Shadow(offset: Offset(-1, 1), color: Colors.black),
-                    ],
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.pink[200],
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  boxShadow: [
+                    const BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                width: 350,
+                height: 100,
+                child: FittedBox(
+                  child: Text(
+                    animalAtual.nome, // nome do animal
+                    style: const TextStyle(
+                      // estilização do nome do animal
+                      fontSize: 60,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        Shadow(offset: Offset(1, 2), color: Colors.black),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-              children: [
-                Container(
-                  //BOTAO ANTERIOR
-                  decoration: BoxDecoration(
-                    color: indiceAtual > 0
-                        ? Colors.pink[400]
-                        : Colors.grey[100],
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      const BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 8,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  child: IconButton(
-                    onPressed: indiceAtual > 0
-                        ? () {
-                            setState(() {
-                              indiceAtual--;
-                            });
-                          }
-                        : null,
-                    icon: Icon(Icons.navigate_before_rounded),
-                    color: Colors.white,
-                    disabledColor: Colors.grey,
-                  ),
-                ), // FIM BOTAO ANTERIOR
-
-                Container(
-                  // BOTÂO DE SOM!
-                  decoration: BoxDecoration(
-                    color: Colors.pink[400],
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 8,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      player.play(
-                        AssetSource(animalAtual.som),
-                      ); // ao clicar no botão a bliblitoca executa o audio
-                    },
-                    icon: const Icon(
-                      Icons.volume_up_rounded,
-                      color: Colors.white,
-                      size: 60,
+                children: [
+                  Container(
+                    //BOTAO ANTERIOR
+                    decoration: BoxDecoration(
+                      color: indiceAtual > 0
+                          ? Colors.pink[400]
+                          : Colors.grey[100],
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        const BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 2,
+                          offset: Offset(0, 1),
+                        ),
+                      ],
                     ),
-                  ),
-                ), // FIM BOTAO SOM
-                Container(
-                  //BOTAO PROXIMO
-                  decoration: BoxDecoration(
-                    color: indiceAtual < animais.length - 1
-                        ? Colors.pink[400]
-                        : Colors.grey[100],
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      const BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 8,
-                        offset: Offset(0, 4),
+                    child: IconButton(
+                      onPressed: indiceAtual > 0
+                          ? () {
+                              setState(() {
+                                indiceAtual--;
+                              });
+                            }
+                          : null,
+                      icon: Icon(Icons.navigate_before_rounded),
+                      color: Colors.white,
+                      disabledColor: Colors.grey,
+                    ),
+                  ), // FIM BOTAO ANTERIOR
+
+                  Container(
+                    // BOTÂO DE SOM!
+                    decoration: BoxDecoration(
+                      color: Colors.pink[400],
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 8,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        player.play(
+                          AssetSource(animalAtual.som),
+                        ); // ao clicar no botão a bliblitoca executa o audio
+                      },
+                      icon: const Icon(
+                        Icons.volume_up_rounded,
+                        color: Colors.white,
+                        size: 60,
                       ),
-                    ],
-                  ),
-                  child: IconButton(
-                    onPressed: indiceAtual < (animais.length - 1)
-                        ? () {
-                            setState(() {
-                              indiceAtual++;
-                            });
-                          }
-                        : null,
-                    icon: Icon(Icons.navigate_next_rounded),
-                    color: Colors.white,
-                    disabledColor: Colors.grey,
-                  ),
-                ), // FIM BOTAO PROXIMO
-              ],
-            ),
-          ],
+                    ),
+                  ), // FIM BOTAO SOM
+                  Container(
+                    //BOTAO PROXIMO
+                    decoration: BoxDecoration(
+                      color: indiceAtual < animais.length - 1
+                          ? Colors.pink[400]
+                          : Colors.grey[100],
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        const BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 8,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: IconButton(
+                      onPressed: indiceAtual < (animais.length - 1)
+                          ? () {
+                              setState(() {
+                                indiceAtual++;
+                              });
+                            }
+                          : null,
+                      icon: Icon(Icons.navigate_next_rounded),
+                      color: Colors.white,
+                      disabledColor: Colors.grey,
+                    ),
+                  ), // FIM BOTAO PROXIMO
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
